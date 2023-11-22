f56f080d89943fe2dbad708897005f3f52e34b43
LamBao <baolam.nguyen2610@gmail.com>
2023-11-22 20:51:52 +0700

    add font, splash image, config project packages

---
+++ lib/configs/themes/app_themes.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+ThemeData theme() {
+  return ThemeData(
+    scaffoldBackgroundColor: Colors.white,
+    fontFamily: 'Muli',
+    textTheme: textTheme(),
+    appBarTheme: appBarTheme(),
+  );
+}
+
+AppBarTheme appBarTheme() {
+  return const AppBarTheme(
+    color: Colors.white,
+    elevation: 0,
+    centerTitle: true,
+    iconTheme: IconThemeData(color: Colors.black),
+    titleTextStyle: TextStyle(
+      color: Colors.black,
+      fontSize: 18,
+      fontWeight: FontWeight.bold,
+    ),
+  );
+}
+
+TextTheme textTheme() {
+  return const TextTheme(
+    titleLarge: TextStyle(
+      height: textHeight,
+      fontSize: 20,
+      fontWeight: FontWeight.bold,
+    ),
+    titleMedium: TextStyle(
+      height: textHeight,
+      fontSize: 18,
+      fontWeight: FontWeight.bold,
+    ),
+    titleSmall: TextStyle(
+      height: textHeight,
+      fontSize: 16,
+      fontWeight: FontWeight.bold,
+    ),
+    bodyLarge: TextStyle(
+      height: textHeight,
+      fontSize: 18,
+    ),
+    bodyMedium: TextStyle(
+      height: textHeight,
+      fontSize: 16,
+    ),
+    bodySmall: TextStyle(
+      height: textHeight,
+      fontSize: 14,
+    ),
+  );
+}
+
+const double textHeight = 1.2;
